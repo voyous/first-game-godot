2ec801dab92d0e8f5a34754733cd6e96d250ea6a
voyous <voyousit@gmail.com>
2025-08-12 05:47:06 +1000

    updated merchant dialogue
    - added check for coins
    - updated merchant dialogue

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,7 +2,14 @@ extends Node
 
 var score = 0;
 @onready var score_label: Label = $ScoreLabel;
+@onready var merchant_text_box: Label = $merchantTextBox
 
 func add_point() -> void:
 	score += 1;
 	score_label.text = "You collected " + str(score) + " coins";
+
+func update_merch_text(text):
+	merchant_text_box.text = text;
+
+func check_score():
+	return score;
--- scripts/merchant.gd
+++ scripts/merchant.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 @onready var timer: Timer = $Timer
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var merchant_text_box: Label = $merchantTextBox
+# @onready var merchant_text_box: Label = $merchantTextBox
+@onready var game_manager: Node = %GameManager
 
 var rand = RandomNumberGenerator.new();
 var is_talking = false;
@@ -26,17 +27,19 @@ func _process(delta: float) -> void:
 	if is_colliding:
 		if Input.is_action_just_pressed("interact"):
 			timer.stop();
-			merchant_text_box.text = "I found a sword on the way here... It's yours for 13 coins.";
+			if game_manager.check_score() < 13:
+				game_manager.update_merch_text("I'm sorry, you need " + str(13 - game_manager.check_score()) + " more coins.");
+			
 			animated_sprite.stop();
 
 # On collision body entered, change text and set colliding to true
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	merchant_text_box.text = "Press E to interact.";
+	game_manager.update_merch_text("Press E to interact.");
 	is_colliding = true;
 
 # On collision body exited, change text to default, start timer again and set colliding to false
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	merchant_text_box.text = "Weapons for sale!";
+	game_manager.update_merch_text("Weapons for sale!");
 	timer.start();
 	animated_sprite.play("default");
 	is_colliding = false;
