237031e84e8d55a6315934417e4d94826ab6e4f7
voyous <voyousit@gmail.com>
2025-08-12 05:02:44 +1000

    Added intractability to merchant
    - removed look around animation stopping on collision
    - added interact to talk to merchant
    - added animation stop on interact

--- scripts/merchant.gd
+++ scripts/merchant.gd
@@ -12,6 +12,7 @@ var is_colliding = false;
 func _ready() -> void:
 	timer.start(rand.randf_range(0.1, 1.5));
 
+# On timer end flip sprite and start new random length timer
 func _on_timer_timeout() -> void:
 	if animated_sprite.flip_h == true:
 		animated_sprite.flip_h = false;
@@ -21,19 +22,21 @@ func _on_timer_timeout() -> void:
 	timer.start(rand.randf_range(0.5, 3));
 
 func _process(delta: float) -> void:
+	# If colliding player can interact to talk to merchant
 	if is_colliding:
-		pass;
-	else:
-		pass;
+		if Input.is_action_just_pressed("interact"):
+			timer.stop();
+			merchant_text_box.text = "I found a sword on the way here... It's yours for 13 coins.";
+			animated_sprite.stop();
 
-
+# On collision body entered, change text and set colliding to true
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	merchant_text_box.text = "Press E to interact.";
-	timer.stop();
 	is_colliding = true;
 
-
+# On collision body exited, change text to default, start timer again and set colliding to false
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	merchant_text_box.text = "Weapons for sale!";
 	timer.start();
+	animated_sprite.play("default");
 	is_colliding = false;
