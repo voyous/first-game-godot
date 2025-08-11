cf713de77c8ee3f19c56e89758105c1476191c0d
voyous <voyousit@gmail.com>
2025-08-12 03:39:56 +1000

    added death animation and jump sound
    - increased death time before restart
    - added death animation
    - removed falling off map on death
    - added jump sound

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -5,7 +5,7 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	print("You Died");
 	Engine.time_scale = 0.5;
-	body.get_node("CollisionShape2D").queue_free();
+	# body.get_node("CollisionShape2D").queue_free();
 	timer.start();
 
 
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,8 +3,14 @@ extends CharacterBody2D
 
 const SPEED = 140
 const JUMP_VELOCITY = -300.0
+var is_dead = false;
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump_sound: AudioStreamPlayer2D = $jumpSound
+
+func die():
+	is_dead = true;
+	animated_sprite.play("death");
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,6 +20,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sound.play();
 
 	# Get input direction: -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
@@ -25,7 +32,11 @@ func _physics_process(delta: float) -> void:
 		animated_sprite.flip_h = true;
 	
 	# Play animations:
-	if is_on_floor():
+	if is_dead:
+		pass;
+	elif Engine.time_scale == 0.5:
+		die();
+	elif is_on_floor():
 		if direction == 0:
 			animated_sprite.play("idle");
 		else:
